85aca7b9eee33f038193ce800b369b178c3f4ad8
Bas <mega@ioexception.at>
2018-02-11 00:00:21 +0100

    Move command cache to instance and file cache to object

--- src/CommandParser.js
+++ src/CommandParser.js
@@ -6,13 +6,21 @@ import { getInstalledPathSync as nodePath } from 'get-installed-path';
 import fs from 'fs';
 import * as path from 'path';
 
-const _fileCache = {};
-const _commandCache = {};
-
 /**
  * Command parser used for looking up commands in the xml definition
  */
 export default class CommandParser {
+  /**
+   * CommandParser constructor
+   */
+  constructor() {
+    if (typeof CommandParser._fileCache === 'undefined') {
+      CommandParser._fileCache = {};
+    }
+
+    this._commandCache = {};
+  }
+
   /**
    * Get an xml file and convert it to json
    * @param {string} name - Project name
@@ -26,20 +34,20 @@ export default class CommandParser {
       throw new Error(`Xml file ${name} could not be found`);
     }
 
-    if (typeof _fileCache[name] === 'undefined') {
-      _fileCache[name] = null;
+    if (typeof CommandParser._fileCache[name] === 'undefined') {
+      CommandParser._fileCache[name] = null;
 
       parseString(file, { async: false }, (e, result) => {
-        _fileCache[name] = result;
+        CommandParser._fileCache[name] = result;
       });
 
       return this._getJson(name);
-    } else if (_fileCache[name] === null) {
+    } else if (CommandParser._fileCache[name] === null) {
       // Fuck javascript async hipster shit
       return this._getJson(name);
     }
 
-    return _fileCache[name];
+    return CommandParser._fileCache[name];
   }
 
   /**
@@ -61,7 +69,7 @@ export default class CommandParser {
       commandName,
     ].join('-');
 
-    if (typeof _commandCache[cacheToken] === 'undefined') {
+    if (typeof this._commandCache[cacheToken] === 'undefined') {
       const project = this._getJson(projectName).project;
 
       this._assertElementExists(project, 'project', projectName);
@@ -80,14 +88,14 @@ export default class CommandParser {
 
       const result = new DroneCommand(project, targetClass, targetCommand);
 
-      _commandCache[cacheToken] = result;
+      this._commandCache[cacheToken] = result;
 
       if (result.deprecated) {
         Logger.warn(`${result.toString()} has been deprecated`);
       }
     }
 
-    const target = _commandCache[cacheToken].clone();
+    const target = this._commandCache[cacheToken].clone();
 
     for (const arg of Object.keys(commandArguments)) {
       if (target.hasArgument(arg)) {
@@ -112,7 +120,7 @@ export default class CommandParser {
     const cacheToken = [projectId, classId, commandId].join('-');
 
     // Build command if needed
-    if (typeof _commandCache[cacheToken] === 'undefined') {
+    if (typeof this._commandCache[cacheToken] === 'undefined') {
       // Find project
       const project = CommandParser._files
         .map(x => this._getJson(x).project)
@@ -136,10 +144,10 @@ export default class CommandParser {
       this._assertElementExists(targetCommand, 'command', commandId, context);
 
       // Build command and store it
-      _commandCache[cacheToken] = new DroneCommand(project, targetClass, targetCommand);
+      this._commandCache[cacheToken] = new DroneCommand(project, targetClass, targetCommand);
     }
 
-    return _commandCache[cacheToken].clone();
+    return this._commandCache[cacheToken].clone();
   }
 
   /**
