10e01e00b0d427dfe3d3b00b9ab1e71b595d138c
Bas <mega@ioexception.at>
2018-04-03 16:59:29 +0200

    Add new index

---
+++ index.js
@@ -0,0 +1,9 @@
+const InvalidCommandError = require('./src/InvalidCommandError');
+const CommandParser = require('./src/CommandParser');
+const DroneConnection = require('./src/DroneConnection');
+
+module.exports = {
+  InvalidCommandError,
+  CommandParser,
+  DroneConnection,
+};
